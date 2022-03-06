c25e420284d640e1ec3567ec5df1c010f0a7e858
anna <anna.jablonska18@gmail.com>
2022-03-06 09:37:49 +0100

    modele

--- AnimalsTeam/models.py
+++ AnimalsTeam/models.py
@@ -1,3 +1,93 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+ROLE = (
+    (1, "inspektor"),
+    (2, "volunteer"),
+)
+
+SPECIES = (
+    (1, 'dog'),
+    (2, 'cat'),
+    (3, 'rabit'),
+    (4, 'horse'),
+    (5, 'bird'),
+    (6, 'goat'),
+    (6, 'cow'),
+)
+HEALTH = (
+    (1, "healthy"),
+    (2, "chronically sick"),
+    (3, "seriously ill"),
+    (4, "during treatment"),
+    (5, "moribund"),
+)
+
+STATUS_ADOPTION = (
+    (1, 'waiting for adoption'),
+    (2, 'in the middle of trying to adopt'),
+    (3, 'not to adoption'),
+    (4, 'adopted'),
+)
+
+STATUS_ADOPTER = (
+    (1, 'in the middle of trying to adopt'),
+    (2, 'adopted'),
+)
+
+SPECIALIZATION = (
+    (1, 'pets permitted'),
+    (2, 'exotic pets'),
+    (3, 'livestock'),
+)
+
+TYPE_OF_FOOD = (
+    (1, "emergency animal feed"),
+    (2, "animal feed")
+)
+
+class Animals(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    species = models.IntegerField(choices=SPECIES)
+    health = models.IntegerField(choices=HEALTH)
+    status = models.IntegerField(choices=STATUS_ADOPTION)
+
+class Personel(models.Model):
+    name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=120)
+    role = models.IntegerField(choices=ROLE)
+    join_date = models.DateTimeField(blank=False)
+    description = models.CharField(max_length=500)
+    animals = models.ManyToManyField(Animals, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Adopter(models.Model):
+    name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=120)
+    address = models.CharField(max_length=120)
+    animal = models.ManyToManyField(Animals, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Adoption(models.Model):
+    animal = models.ForeignKey(Animals, on_delete=models.CASCADE)
+    adopter = models.ForeignKey(Adopter, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS_ADOPTER)
+    date_adoption = models.DateField()
+
+class Vet(models.Model):
+    name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=120)
+    specialization = models.IntegerField(choices=SPECIALIZATION)
+    animals = models.ManyToManyField(Animals, blank=True)
+
+class Food(models.Model):
+    name = models.CharField(max_length=60)
+    type = models.IntegerField(choices=TYPE_OF_FOOD)
+    stan = models.IntegerField()
+
+
+
+
+
